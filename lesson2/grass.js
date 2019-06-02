3c4de641ca76f5964a1083d4b22f41dbf8f463fd
user <you@example.com>
2019-06-02 15:53:25 +0400

    eguryfgeyfgey

---
+++ lesson2/grass.js
@@ -0,0 +1,38 @@
+class Grass  extends LivingCreature{
+    //շրջապատի հետազոտության մատրիցը
+    newDirections() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    } 
+
+    //mul() Բազմացում
+    mul() {
+        this.multiply++;
+        if (this.multiply == 7) {
+
+            //Հետազոտում է շրջապատը, որոնում դատարկ տարածքներ
+            var fundCords = this.getDirections(0);
+            var cord = random(fundCords);
+            if (cord) {
+                var x = cord[0];
+                var y = cord[1];
+
+                //Ավելացնում է նոր խոտ խոտերի զանգվածում
+                var newgrass = new Grass(x, y);
+                grassArr.push(newgrass);
+
+                //Ավելացնում է նոր խոտի մասին գրառում հիմնական matrix-ում 
+                matrix[y][x] = 1;
+                this.multiply = 0;
+            }
+        }
+    }
+}
